44a64f4d2c3eaccf207d730ddef8bc0b228b7c07
Sriram <s.padavettan@gmail.com>
2017-11-08 12:29:26 -0800

    first commit

---
+++ src/edu/ucsd/cs110/temperature/Celsius.java
@@ -0,0 +1,25 @@
+package edu.ucsd.cs110.temperature;
+
+public class Celsius extends Temperature
+{
+    public Celsius(float t)
+    {
+        super(t);
+    }
+
+    @Override
+    public Temperature toCelsius() {
+        return null;
+    }
+
+    @Override
+    public Temperature toFahrenheit() {
+        return null;
+    }
+
+    public String toString()
+    {
+        // TODO: Complete this method
+        return "";
+    }
+}
